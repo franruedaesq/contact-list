80299e1cde20534370ff031ef232e4ae69cfcc25
franruedaesq <franruedaesq@gmail.com>
2021-07-25 20:42:06 -0300

    catch API error

--- src/services/users.js
+++ src/services/users.js
@@ -1,9 +1,13 @@
 import { configJson } from 'configuration';
 
 export const fetchData = async () => {
-  const resp = await fetch(
-    `${configJson.userUrl}?results=${configJson.numberCards}`
-  );
-  const data = await resp.json();
-  return data;
+  try {
+    const resp = await fetch(
+      `${configJson.userUrl}?results=${configJson.numberCards}`
+    );
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+      console.log(error)
+  }
 };
